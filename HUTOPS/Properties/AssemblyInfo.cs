2ca6a46ffdc5491c21876e24f126b3c73cf7e1ac
Muhammad Zaid <muhammad.zaid@habib.edu.pk>
2023-08-28 16:43:29 +0500

    -- Update Namespaces

--- HUTOPS/Properties/AssemblyInfo.cs
+++ HUTOPS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HU.TOP.WebApp")]
+[assembly: AssemblyTitle("HUTOPS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HU.TOP.WebApp")]
+[assembly: AssemblyProduct("HUTOPS")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
