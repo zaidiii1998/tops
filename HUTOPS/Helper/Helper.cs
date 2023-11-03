6def3953ef1d7cad62d24a3fda073c107a1a9d1e
Muhammad Zaid <muhammad.zaid@habib.edu.pk>
2023-11-03 19:12:56 +0500

    Update Regix for validate email

--- HUTOPS/Helper/Helper.cs
+++ HUTOPS/Helper/Helper.cs
@@ -178,7 +178,7 @@ namespace HUTOPS.Helper
         }
         public static bool isValidEmail(string inputEmail)
         {
-            string strRegex = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            string strRegex = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";//@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
             Regex re = new Regex(strRegex);
             if (re.IsMatch(inputEmail))
                 return (true);
